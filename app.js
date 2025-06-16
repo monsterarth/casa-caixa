2267737f26d26c4bca5cac65535dad3864957c71
monsterarth <47015839+monsterarth@users.noreply.github.com>
2025-06-16 14:21:50 -0300

    seeding

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-// PASSO 1: COLE AQUI O SEU OBJETO firebaseConfig DO FIREBASE
+// Configuração do firebase
 const firebaseConfig = {
-    // apiKey: "...",
-    // authDomain: "...",
-    // ...
+  apiKey: "AIzaSyBa9ptFhgKmO0DvicaemuLKPn_kcb94XXU",
+  authDomain: "casa-e-caixa.firebaseapp.com",
+  projectId: "casa-e-caixa",
+  storageBucket: "casa-e-caixa.firebasestorage.app",
+  messagingSenderId: "210887017905",
+  appId: "1:210887017905:web:c59e8cd3ea04bdcbcbe990",
+  measurementId: "G-6LQY4W77XF"
 };
 
 // Inicializa o Firebase
